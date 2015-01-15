5757107adec9bf2f63d792842b0d5b01bdccd4d5
Iranid <iranidperez@gmail.com>
2015-01-15 01:03:40 -0430

    Borrador 1.1 tarea 2

--- Reserva/reservacion.py
+++ Reserva/reservacion.py
@@ -3,9 +3,10 @@ Created on 12/1/2015
 
 @author: Iranid
 '''
-from datetime import date,datetime, time
+from datetime import date,datetime, time, timedelta
 import sys 
 from tarifa import Tarifa
+from django.forms.formsets import TOTAL_FORM_COUNT
 
 def main():
     '''El cableado de datos es temporal '''
@@ -37,19 +38,65 @@ def main():
         sys.exit()
     
     tarifa = Tarifa(parametro_tasa_diurna, parametro_tasa_nocturna)
+    
+    monto_total= calculo_monto_reserva(fecha_entrada, fecha_salida, tarifa)
+    print("El monto total a pagar por la reserva es: ", monto_total)
+    
+    
+    
 
-def calculo_monto_reserva(self, fecha_entrada, fecha_salida,tarifa):
+def calculo_monto_reserva(fecha_entrada, fecha_salida,tarifa):
+    
+    total_pago_reserva = 0
+    
     
     #Determina si fecha de entrada es mayor a la fecha de entrada
     restaDias = fecha_salida - fecha_entrada
     
     if restaDias.days < 0:
         print("La fecha de entrada es mayor que la fecha de salida")
+        sys.exit()
         
-    if restaDias.total_seconds() <(15*60) and restaDias.seconds >(72*60*60):
+    if restaDias.total_seconds() <(15*60) or restaDias.total_seconds() >(72*60*60):
         print("La reservación no cumple los límites de tiempo inferior y superior")
+        sys.exit()
+    
+    max_tasa = max(tarifa.__tasa_diurna, tarifa.__tasa_nocturna)
+    
+    fecha_revision = fecha_entrada
+    while fecha_revision < fecha_salida:
+        
+        hora_actual = fecha_revision.hora
+        hora_siguiente = (hora_actual + 1) % 24        
+        
+        periodo_nocturno1 = list(range(0,6))
+        if hora_actual in periodo_nocturno1:
+            if hora_siguiente == 6:
+                total_pago_reserva += max_tasa
+            else:
+                total_pago_reserva += tarifa.__tasa_nocturna
+            
+    
+        
+        periodo_diurno = list(range(6,18))
+        if hora_actual in periodo_diurno:
+            if hora_siguiente == 18:
+                total_pago_reserva += max_tasa
+            else:
+                total_pago_reserva += tarifa.__tasa_diurna
+            
+        
+        periodo_nocturno2 = list(range(18, 24))
+        if hora_actual in periodo_nocturno2:
+            total_pago_reserva += tarifa.__tasa_nocturna
     
     
+        fecha_revision = fecha_revision.replace(hour = hora_siguiente, minute = fecha_salida.minute)
+        if fecha_revision.hour == 0:
+            fecha_revision = fecha_revision + timedelta(days = 1)
+    
+    return total_pago_reserva
+        
     
     
 if __name__== "__main__":
