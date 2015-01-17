a409a2000b9a76437a8d7588b3fd1e4f122ea2dc
Jonnathan Ng <ngjonnathan@gmail.com>
2015-01-17 17:27:02 -0430

    Version agregado casos de prueba

--- Reserva/casosPrueba.py
+++ Reserva/casosPrueba.py
@@ -41,10 +41,45 @@ class TestHoras(unittest.TestCase):
     def testMayorTiempoExacto(self):
         monto = self.calcularMonto("2014-01-01 01:00", "2014-01-04 01:00")
         self.assertEqual(monto,(nocturno*12 + diurno*12)*3) 
-                 
+
     def testUnMinuto(self):
         monto = self.calcularMonto("2014-01-01 01:00","2014-01-01 01:16")
         self.assertEqual(monto, nocturno*1)
+        
+    def testTransiciones(self):
+        monto = self.calcularMonto("2014-01-01 05:01", "2014-01-01 6:01")
+        self.assertEqual(monto, max(nocturno,diurno))
+        
+        monto = self.calcularMonto("2014-01-01 17:01", "2014-01-01 18:01")
+        self.assertEqual(monto, max(nocturno,diurno))
+        
+    def testTransicionSinMaximo(self):
+        monto = self.calcularMonto("2014-01-01 05:00", "2014-01-01 6:00")
+        self.assertEqual(monto, nocturno)
+        monto = self.calcularMonto("2014-01-01 17:00", "2014-01-01 18:00")
+        self.assertEqual(monto, diurno)
+        
+    def testHoraDemas(self):
+        monto = self.calcularMonto("2014-01-01 20:00", "2014-01-01 21:01")
+        self.assertEqual(monto, nocturno*2)
+        monto = self.calcularMonto("2014-01-01 8:00", "2014-01-01 9:01")
+        self.assertEqual(monto, diurno*2)
+        
+    def testHoraDemasConTransicion(self):
+         #Recomendacion: Prueba cambiando valores de nocturno y diurno
+        #Caso nocturno > diurno, diurno > nocturno (habria q cambiar la funcion)
+        monto = self.calcularMonto("2014-01-01 5:01", "2014-01-01 6:02")
+        self.assertEqual(monto, max(nocturno,diurno) + diurno)
+        monto = self.calcularMonto("2014-01-01 17:01", "2014-01-01 18:02")
+        self.assertEqual(monto, max(nocturno,diurno) + nocturno)
+        
+    def testHoraDemasConTransicion2(self):
+        #Recomendacion: Prueba cambiando valores de nocturno y diurno
+        #Caso nocturno > diurno, diurno > nocturno (habria q cambiar la funcion)
+        monto = self.calcularMonto("2014-01-01 5:00", "2014-01-01 6:01")
+        self.assertEqual(monto,nocturno + diurno)
+        monto = self.calcularMonto("2014-01-01 17:00", "2014-01-01 18:01")
+        self.assertEqual(monto,nocturno + diurno)
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
